715e9cf4373d2a705e2f9ad5519eed11281612bf
eubank87 <eubank.ashley@gmail.com>
2021-01-23 10:30:42 -0800

    added seeds file. Fires successfully in workbench, Table created

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (name)
+VALUES ("New Bacon-ings Burger"),
+("Foot Feta-ish Burger"),
+("Butterface Burger"),
+("she's a Super Leek Burger");
+
+INSERT INTO burgers (name, devoured)
+VALUES ("Chile Relleno-You-Didn't Burger", true),
+("Cheeses is Born Burger", true);
