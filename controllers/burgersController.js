d20016bbcf76ca2a4e2f28c188b015c6b741e9cd
eubank87 <eubank.ashley@gmail.com>
2021-01-23 12:21:04 -0800

    added delete route to controllers file

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -8,7 +8,7 @@ const burger = require("../models/burger.js");
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.all(function(data) {
-    var hbsObject = {
+    const hbsObject = {
       burgers: data
     };
     console.log(hbsObject);
@@ -18,7 +18,7 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
   burger.create([
-    "name", "devouored"
+    "name", "devoured"
   ], [
     req.body.name, req.body.devoured
   ], function(result) {
@@ -28,14 +28,27 @@ router.post("/api/burgers", function(req, res) {
 });
 
 router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+  const condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  cat.update({
+  burger.update({
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+  const condition = "id = " + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
